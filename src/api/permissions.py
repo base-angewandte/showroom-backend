869b496163fed8380b1c8f3484715d24e92ab24e
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2021-10-21 09:38:43 +0200

    ~ fix #1565 and improve ActivityPermission
    adds PUT and PATCH although not yet needed. but it is the secure default
    now if we add those methods later.

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -4,15 +4,21 @@ from core.models import SourceRepository
 
 
 class ActivityPermission(permissions.BasePermission):
-    """Checks that only source repos may POST and DELETE activities."""
+    """Checks that only source repos may create and modify activities."""
 
     def has_permission(self, request, view):
-        if request.method in ('POST', 'DELETE'):
+        if request.method in ('POST', 'PUT', 'PATCH', 'DELETE'):
             if (
                 'X-Api-Client' not in request.headers
                 or 'X-Api-Key' not in request.headers
             ):
                 return False
+            if (
+                request.headers['X-Api-Client'] == ''
+                or request.headers['X-Api-Key'] == ''
+            ):
+                return False
+
             try:
                 repo = SourceRepository.objects.get(pk=request.headers['X-Api-Client'])
             except SourceRepository.DoesNotExist:
