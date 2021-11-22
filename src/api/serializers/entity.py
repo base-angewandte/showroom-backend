ec66456778f70265a0a192082fa6cc3ef9b5b760
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2021-11-22 11:03:13 +0100

    + add showcase serialisation to EntitySerializer

--- src/api/serializers/entity.py
+++ src/api/serializers/entity.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from core.models import Entity
 
 from . import abstract_showroom_object_fields
+from .showcase import get_serialized_showcase_and_warnings
 
 
 class EntitySerializer(serializers.ModelSerializer):
@@ -15,3 +16,16 @@ class EntitySerializer(serializers.ModelSerializer):
             'photo',
             'parent',
         ]
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        # make sure to only provide an empty list if no showcase is None or {}
+        if not instance.showcase:
+            instance.showcase = []
+        sc, sc_warnings = get_serialized_showcase_and_warnings(instance.showcase)
+        ret['showcase'] = sc
+        if sc_warnings:
+            ret['showcase_warnings'] = sc_warnings
+
+        return ret
