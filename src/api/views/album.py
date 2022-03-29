fc0afa4acf9922170c0ba574d762db1ff2723828
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2022-03-29 15:14:25 +0200

    ~ adopt AlbumViewSet scaffold to new model

--- src/api/views/album.py
+++ src/api/views/album.py
@@ -1,32 +1,46 @@
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from api.serializers.album import AlbumSerializer
 from api.serializers.generic import Responses
 from core.models import ShowroomObject
 
 
-@extend_schema_view(
-    create=extend_schema(
+class AlbumViewSet(viewsets.GenericViewSet):
+    queryset = ShowroomObject.objects.filter(type=ShowroomObject.ALBUM)
+    serializer_class = AlbumSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    @extend_schema(exclude=True)
+    def list(self, request, *args, **kwargs):
+        # Similar to list in EntityViewSet
+        raise MethodNotAllowed(method='GET')
+
+    @extend_schema(
+        tags=['public'],
+        responses={
+            200: AlbumSerializer,
+            404: Responses.Error404,
+        },
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return Response({'detail': 'not yet implemented'})
+
+    @extend_schema(
         tags=['auth'],
         responses={
             201: AlbumSerializer,
             400: Responses.Error400,
             403: Responses.Error403,
         },
-    ),
-    partial_update=extend_schema(
-        tags=['auth'],
-        responses={
-            200: AlbumSerializer,
-            400: Responses.Error400,
-            403: Responses.Error403,
-            404: Responses.Error404,
-        },
-    ),
-    destroy=extend_schema(
+    )
+    def create(self, request, *args, **kwargs):
+        return Response({'detail': 'not yet implemented'})
+
+    @extend_schema(
         tags=['auth'],
         responses={
             204: None,
@@ -34,22 +48,18 @@ from core.models import ShowroomObject
             403: Responses.Error403,
             404: Responses.Error404,
         },
-    ),
-    retrieve=extend_schema(
-        tags=['public'],
+    )
+    def destroy(self, request, *args, **kwargs):
+        return Response({'detail': 'not yet implemented'})
+
+    @extend_schema(
+        tags=['auth'],
         responses={
             200: AlbumSerializer,
+            400: Responses.Error400,
+            403: Responses.Error403,
             404: Responses.Error404,
         },
-    ),
-    update=extend_schema(exclude=True),
-)
-class AlbumViewSet(viewsets.ModelViewSet):
-    queryset = ShowroomObject.objects.filter(type=ShowroomObject.ALBUM)
-    serializer_class = AlbumSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    @extend_schema(exclude=True)
-    def list(self, request, *args, **kwargs):
-        # Similar to list in EntitiyViewSet
-        raise MethodNotAllowed(method='GET')
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return Response({'detail': 'not yet implemented'})
