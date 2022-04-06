0560abaca2a4acd8b39eddf80f72a1b8a88a13e3
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2022-04-06 11:19:41 +0200

    + add more usable admin model for ShowroomObject

--- src/core/admin.py
+++ src/core/admin.py
@@ -18,8 +18,24 @@ class SourceRepoAdmin(admin.ModelAdmin):
     repo_label.short_description = 'Source repository'
 
 
+class ShowroomObjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'type',
+        'title',
+        'subtext',
+        'belongs_to',
+        'source_repo',
+        'date_created',
+        'date_changed',
+        'date_synced',
+    )
+    list_filter = ('type', 'source_repo')
+    search_fields = ('title', 'subtext', 'source_repo_data')
+
+
 admin.site.register(SourceRepository, SourceRepoAdmin)
-admin.site.register(ShowroomObject)
+admin.site.register(ShowroomObject, ShowroomObjectAdmin)
 admin.site.register(ActivityDetail)
 admin.site.register(EntityDetail)
 admin.site.register(Media)
