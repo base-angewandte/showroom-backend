42c91a5cc727a5181dee0c9b5f01a682bff59a6a
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-04-15 09:51:49 +0200

    ~ changed ActivityPermission to check only for Api Key

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -10,29 +10,14 @@ class ActivityPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.method in ('POST', 'PUT', 'PATCH', 'DELETE'):
-            if (
-                'X-Api-Client' not in request.headers
-                or 'X-Api-Key' not in request.headers
-            ):
-                return False
-            if (
-                request.headers['X-Api-Client'] == ''
-                or request.headers['X-Api-Key'] == ''
-            ):
+            if not request.headers.get('X-Api-Key'):
                 return False
 
             try:
-                repo = SourceRepository.objects.get(pk=request.headers['X-Api-Client'])
+                SourceRepository.objects.get(api_key=request.headers['X-Api-Key'])
             except SourceRepository.DoesNotExist:
                 return False
-            if request.headers['X-Api-Key'] != repo.api_key:
-                return False
 
-            # check whether the data contains a `source_repo` key and only
-            # allow the operation if its value is the same
-            if source_repo := request.data.get('source_repo'):
-                if source_repo != repo.id:
-                    return False
         return True
 
 
