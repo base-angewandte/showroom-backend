b4060db79e91adef42ca509ff70317591686260e
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-04-15 09:55:37 +0200

    ~ flake8 change

--- src/api/views/entity.py
+++ src/api/views/entity.py
@@ -264,7 +264,7 @@ class EntityViewSet(viewsets.GenericViewSet):
                 try:
                     validate_showcase(instance.entitydetail.showcase)
                 except ValidationError as err:
-                    raise serializers.ValidationError({'showcase': err})
+                    raise serializers.ValidationError({'showcase': err}) from err
                 instance.entitydetail.save()
             if (secondary_details := data.get('secondary_details')) is not None:
                 instance.secondary_details = secondary_details
