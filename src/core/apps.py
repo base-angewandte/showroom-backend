a53abc84e75c6f097d968632488ab6e3f44dc9af
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-04-21 12:57:01 +0200

    + update dynamic filter cache via rq scheduler

--- src/core/apps.py
+++ src/core/apps.py
@@ -1,5 +1,40 @@
+import django_rq
+
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    scheduler = django_rq.get_scheduler('high')
+
+    jobs = [
+        {
+            'id': 'get_dynamic_filters_de',
+            'schedule': '*/25 * * * *',
+            'function': 'api.views.filter.get_dynamic_filters',
+            'kwargs': {
+                'lang': 'de',
+                'use_cache': False,
+            },
+        },
+        {
+            'id': 'get_dynamic_filters_en',
+            'schedule': '*/25 * * * *',
+            'function': 'api.views.filter.get_dynamic_filters',
+            'kwargs': {
+                'lang': 'en',
+                'use_cache': False,
+            },
+        },
+    ]
+
+    for job in jobs:
+        if job['id'] not in scheduler:
+            scheduler.cron(
+                job['schedule'],
+                job['function'],
+                id=job['id'],
+                timeout=7200,
+                kwargs=job['kwargs'],
+            )
