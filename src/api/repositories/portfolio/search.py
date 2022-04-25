3d18f8482a2f4d2e54c2c59fd12aaaf322a93c9c
Philipp Mayer <philipp.mayer@paradoxon.at>
2022-04-25 04:32:20 +0200

    + cache search item

--- src/api/repositories/portfolio/search.py
+++ src/api/repositories/portfolio/search.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.conf import settings
+from django.core.cache import cache
 from django.utils.text import slugify
 
 from core.models import ShowroomObject
@@ -18,129 +19,136 @@ def gather_labels(items):
 
 
 def get_search_item(item, lang=settings.LANGUAGES[0][0]):
-    search_item = {
-        'id': item.id,
-        'type': None,
-        'title': None,
-        'subtitle': None,
-        'description': None,
-        'alternative_text': [],
-        'image_url': None,
-        'source_institution': {
-            'label': item.source_repo.label_institution,
-            'url': item.source_repo.url_institution,
-            'icon': item.source_repo.icon,
-        },
-        'score': 0,
-    }
-    if hasattr(item, 'rank'):
-        search_item['score'] = item.rank
+    cache_key = f'get_search_item_{item.id}_{lang}'
 
-    if item.type == ShowroomObject.ACTIVITY:
-        search_item['type'] = 'activity'
-    elif item.type == ShowroomObject.ALBUM:
-        search_item['type'] = 'album'
-    else:
-        search_item['id'] = slugify(item.title) + '-' + item.id
-        # TODO: refactor this (also in entity serializer, to be configurable)
-        if item.type == ShowroomObject.PERSON:
-            search_item['type'] = 'person'
-        elif item.type == ShowroomObject.INSTITUTION:
-            search_item['type'] = 'institution'
-        elif item.type == ShowroomObject.DEPARTMENT:
-            search_item['type'] = 'department'
+    search_item = cache.get(cache_key)
+    if not search_item:
+        search_item = {
+            'id': item.id,
+            'type': None,
+            'title': None,
+            'subtitle': None,
+            'description': None,
+            'alternative_text': [],
+            'image_url': None,
+            'source_institution': {
+                'label': item.source_repo.label_institution,
+                'url': item.source_repo.url_institution,
+                'icon': item.source_repo.icon,
+            },
+            'score': 0,
+        }
+        if hasattr(item, 'rank'):
+            search_item['score'] = item.rank
 
-    if item.type == ShowroomObject.ACTIVITY:
-        # in case a featured medium is set, we'll use this. if non is set explicitly
-        # we search if there is any image attached to the entry and take this one.
-        media = item.media_set.all()
-        featured_medium = media.filter(featured=True)
-        if featured_medium:
-            medium = featured_medium[0]
-            if medium.type == 'v':
-                if cover := medium.specifics.get('cover'):
-                    search_item['image_url'] = cover.get('jpg')
-            else:
-                search_item['image_url'] = medium.specifics.get('thumbnail')
-        if search_item['image_url'] is None:
-            alternative_preview = None
-            for medium in media:
-                if medium.type == 'i':
-                    thumbnail = medium.specifics.get('thumbnail')
-                    if not thumbnail:
-                        continue
-                    search_item['image_url'] = thumbnail
-                    break
-                elif not alternative_preview:
-                    if medium.type == 'v':
-                        if cover := medium.specifics.get('cover'):
-                            if cover_jpg := cover.get('jpg'):
-                                alternative_preview = cover_jpg
-                    else:
-                        alternative_preview = medium.specifics.get('thumbnail')
-            if not search_item['image_url'] and alternative_preview:
-                search_item['image_url'] = alternative_preview
-    elif item.type in [
-        ShowroomObject.PERSON,
-        ShowroomObject.DEPARTMENT,
-        ShowroomObject.INSTITUTION,
-    ]:
-        photo = item.entitydetail.photo
-        search_item['image_url'] = photo if photo else None
-
-    activity_schema = None
-    if (
-        item.type == ShowroomObject.ACTIVITY
-        and item.activitydetail
-        and item.activitydetail.activity_type
-    ):
-        activity_schema = get_schema(item.activitydetail.activity_type.get('source'))
-    mapping = map_search(search_item['type'], activity_schema)
-
-    functions = {
-        'activity_type_university': get_activity_type_university,
-        'architecture_contributors': get_architecture_contributors,
-        'artists_contributors': get_artists_contributors,
-        'artists_curators_contributors': get_artists_curators_contributors,
-        'authors_artists_contributors': get_authors_artists_contributors,
-        'authors_editors': get_authors_editors,
-        'contributors': get_contributors,
-        'design_contributors': get_design_contributors,
-        'developers_contributors': get_developers_contributors,
-        'directors_contributors': get_directors_contributors,
-        'fellow_scholar_funding': get_fellow_scholar_funding,
-        'lecturers_contributors': get_lecturers_contributors,
-        'music_conductors_composition_contributors': get_music_conductors_composition_contributors,
-        'name': get_name,
-        'organisers_artists_curators': get_organisers_artists_curators,
-        'organisers_lecturers_contributors': get_organisers_lecturers_contributors,
-        'project_lead_partners_funding': get_project_lead_partners_funding,
-        'skills': get_skills,
-        'text_keywords': get_text_keywords,
-        'title_subtitle': get_title_subtitle,
-        'university': get_university,
-        'winners_jury_contributors': get_winners_jury_contributors,
-    }
-
-    for field, map_function in mapping.items():
-        if map_function is None:
-            continue
-        if (transform_func := functions.get(map_function)) is None:
-            if settings.DEBUG:
-                # TODO: discuss: do we want this also in prod, or an admin notification?
-                logger.error(
-                    f'Missing search mapping function: {{"{field}": "{map_function}"}}'
-                )
-            continue
-        transformed = transform_func(item, lang)
-        # TODO: discuss: should we actually just filter out None values or should
-        #       we try to get a default value instead, if the localised is not available?
-        transformed = [item for item in transformed if item]
-
-        if field == 'alternative_text':
-            search_item[field] = transformed
+        if item.type == ShowroomObject.ACTIVITY:
+            search_item['type'] = 'activity'
+        elif item.type == ShowroomObject.ALBUM:
+            search_item['type'] = 'album'
         else:
-            search_item[field] = ', '.join(transformed)
+            search_item['id'] = slugify(item.title) + '-' + item.id
+            # TODO: refactor this (also in entity serializer, to be configurable)
+            if item.type == ShowroomObject.PERSON:
+                search_item['type'] = 'person'
+            elif item.type == ShowroomObject.INSTITUTION:
+                search_item['type'] = 'institution'
+            elif item.type == ShowroomObject.DEPARTMENT:
+                search_item['type'] = 'department'
+
+        if item.type == ShowroomObject.ACTIVITY:
+            # in case a featured medium is set, we'll use this. if non is set explicitly
+            # we search if there is any image attached to the entry and take this one.
+            media = item.media_set.all()
+            featured_medium = media.filter(featured=True)
+            if featured_medium:
+                medium = featured_medium[0]
+                if medium.type == 'v':
+                    if cover := medium.specifics.get('cover'):
+                        search_item['image_url'] = cover.get('jpg')
+                else:
+                    search_item['image_url'] = medium.specifics.get('thumbnail')
+            if search_item['image_url'] is None:
+                alternative_preview = None
+                for medium in media:
+                    if medium.type == 'i':
+                        thumbnail = medium.specifics.get('thumbnail')
+                        if not thumbnail:
+                            continue
+                        search_item['image_url'] = thumbnail
+                        break
+                    elif not alternative_preview:
+                        if medium.type == 'v':
+                            if cover := medium.specifics.get('cover'):
+                                if cover_jpg := cover.get('jpg'):
+                                    alternative_preview = cover_jpg
+                        else:
+                            alternative_preview = medium.specifics.get('thumbnail')
+                if not search_item['image_url'] and alternative_preview:
+                    search_item['image_url'] = alternative_preview
+        elif item.type in [
+            ShowroomObject.PERSON,
+            ShowroomObject.DEPARTMENT,
+            ShowroomObject.INSTITUTION,
+        ]:
+            photo = item.entitydetail.photo
+            search_item['image_url'] = photo if photo else None
+
+        activity_schema = None
+        if (
+            item.type == ShowroomObject.ACTIVITY
+            and item.activitydetail
+            and item.activitydetail.activity_type
+        ):
+            activity_schema = get_schema(
+                item.activitydetail.activity_type.get('source')
+            )
+        mapping = map_search(search_item['type'], activity_schema)
+
+        functions = {
+            'activity_type_university': get_activity_type_university,
+            'architecture_contributors': get_architecture_contributors,
+            'artists_contributors': get_artists_contributors,
+            'artists_curators_contributors': get_artists_curators_contributors,
+            'authors_artists_contributors': get_authors_artists_contributors,
+            'authors_editors': get_authors_editors,
+            'contributors': get_contributors,
+            'design_contributors': get_design_contributors,
+            'developers_contributors': get_developers_contributors,
+            'directors_contributors': get_directors_contributors,
+            'fellow_scholar_funding': get_fellow_scholar_funding,
+            'lecturers_contributors': get_lecturers_contributors,
+            'music_conductors_composition_contributors': get_music_conductors_composition_contributors,
+            'name': get_name,
+            'organisers_artists_curators': get_organisers_artists_curators,
+            'organisers_lecturers_contributors': get_organisers_lecturers_contributors,
+            'project_lead_partners_funding': get_project_lead_partners_funding,
+            'skills': get_skills,
+            'text_keywords': get_text_keywords,
+            'title_subtitle': get_title_subtitle,
+            'university': get_university,
+            'winners_jury_contributors': get_winners_jury_contributors,
+        }
+
+        for field, map_function in mapping.items():
+            if map_function is None:
+                continue
+            if (transform_func := functions.get(map_function)) is None:
+                if settings.DEBUG:
+                    # TODO: discuss: do we want this also in prod, or an admin notification?
+                    logger.error(
+                        f'Missing search mapping function: {{"{field}": "{map_function}"}}'
+                    )
+                continue
+            transformed = transform_func(item, lang)
+            # TODO: discuss: should we actually just filter out None values or should
+            #       we try to get a default value instead, if the localised is not available?
+            transformed = [item for item in transformed if item]
+
+            if field == 'alternative_text':
+                search_item[field] = transformed
+            else:
+                search_item[field] = ', '.join(transformed)
+        cache.set(cache_key, search_item, 60 * 5)
 
     return search_item
 
