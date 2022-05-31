c08038596e0390dc2f58447d86c5752bab095248
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2022-05-31 21:10:04 +0200

    + add schema description for repo_source endpoint

--- src/api/plugins/repo_source.py
+++ src/api/plugins/repo_source.py
@@ -1,3 +1,6 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,10 +9,41 @@ from core.models import ShowroomObject
 from general.datetime.utils import format_datetime
 
 
+class RepoSourceSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    subtitle = serializers.CharField(required=False)
+    type = serializers.JSONField(required=False)
+    keywords = serializers.JSONField(required=False)
+    texts = serializers.JSONField(required=False)
+    data = serializers.JSONField(required=False)
+    _showroom_id = serializers.CharField()
+    _publishing_info = serializers.JSONField()
+
+
+@extend_schema(
+    tags=['api_plugin'],
+    parameters=[
+        OpenApiParameter(
+            'id',
+            OpenApiTypes.STR,
+            OpenApiParameter.PATH,
+            description='A unique value identifying this showroom object.',
+        )
+    ],
+)
 class RepoSourceView(APIView):
     permission_classes = [HasPluginAPIKey]
+    serializer_class = RepoSourceSerializer
 
     def get(self, request, *args, **kwargs):
+        """Retrieve activity in the schema originally provided by the
+        repository.
+
+        Properties additionally added by Showroom are preceded with `_`,
+        e.g. the `_showroom_id` or the `_publishing_info`. This endpoint
+        is only available for clients authenticated with an API key
+        configured for the repo_source API plugin.
+        """
         self.check_object_permissions(request, 'repo_source')
 
         try:
