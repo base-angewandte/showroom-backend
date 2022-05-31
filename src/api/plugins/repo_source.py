5a9264950d2c629079019142abbcc0b3c9a21ab4
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2022-05-31 20:35:21 +0200

    + implement RepoSourceView

--- src/api/plugins/repo_source.py
+++ src/api/plugins/repo_source.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.permissions import HasPluginAPIKey
+from core.models import ShowroomObject
+from general.datetime.utils import format_datetime
 
 
 class RepoSourceView(APIView):
@@ -9,4 +11,21 @@ class RepoSourceView(APIView):
 
     def get(self, request, *args, **kwargs):
         self.check_object_permissions(request, 'repo_source')
-        return Response({'detail': 'not yet implemented'}, status=200)
+
+        try:
+            activity = ShowroomObject.active_objects.get(
+                type=ShowroomObject.ACTIVITY,
+                pk=kwargs['pk'],
+            )
+        except ShowroomObject.DoesNotExist:
+            return Response({'detail': 'Not found.'}, status=404)
+
+        ret = activity.source_repo_data
+        ret['_showroom_id'] = activity.showroom_id
+        ret['_publishing_info'] = {
+            'publisher': activity.source_repo_owner_id,
+            'date_published': format_datetime(activity.date_created),
+            'date_updated': format_datetime(activity.date_synced),
+        }
+
+        return Response(ret, status=200)
