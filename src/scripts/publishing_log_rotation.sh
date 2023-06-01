2a1e4008a5ca8f377e01188092b4ba297fa414e5
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2023-06-01 11:04:21 +0200

    chore: apply linting fixes to shell scripts

--- src/scripts/publishing_log_rotation.sh
+++ src/scripts/publishing_log_rotation.sh
@@ -3,8 +3,8 @@
 PUBLOG_ROTATION_LOG=/logs/publishing_log_rotation.log
 
 {
-  echo "$(date): checking retention"
-  python manage.py publishing_log -m retention
-  echo "$(date): compressing rotated files"
-  python manage.py publishing_log -m compress
-} >> $PUBLOG_ROTATION_LOG
+	echo "$(date): checking retention"
+	python manage.py publishing_log -m retention
+	echo "$(date): compressing rotated files"
+	python manage.py publishing_log -m compress
+} >>$PUBLOG_ROTATION_LOG
